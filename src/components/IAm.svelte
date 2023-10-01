1f57c14aebd9ad59ba764d21972fc643bd3c2252
bisaek <huggerbertram@gmail.com>
2023-10-01 13:48:06 +0200

    landing page

---
+++ src/components/IAm.svelte
@@ -0,0 +1,18 @@
+<script>  
+      const citater = [
+        "Jeg er Bertram",
+        "Jeg er web-udvikler",
+        "Jeg vil Hjælpe dig",
+        
+      ]
+      let IAM = citater[0]
+      setInterval(() => {
+            if(citater.indexOf(IAM) + 1 >= citater.length) IAM = citater[0]
+            else IAM = citater[citater.indexOf(IAM) + 1]
+      }, 5000)
+  
+  </script>
+  
+{IAM}
+  
+  
