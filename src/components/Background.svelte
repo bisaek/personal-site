fb497ea13df3d6b2919d56139efa1e76e45384c9
bisaek <huggerbertram@gmail.com>
2023-10-02 15:23:46 +0200

    create a good background

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,30 +1,299 @@
-<canvas bind:this={canvas} class="bg-cover"></canvas>
+<canvas  bind:this={canvas} class="block absolute -z-10 h-full w-full"></canvas>
 
-<script lang="js">
-	import * as THREE from "three";
-    import { onMount } from "svelte"
 
-    let canvas
+<script>
+  import { onMount } from "svelte";
 
-    onMount(() => {
-        createScene(canvas)
-    })
+  /*          *     .        *  .    *    *   . 
+ .  *  move your mouse to over the stars   .
+ *  .  .   change these values:   .  *
+   .      * .        .          * .       */
+const STAR_COLOR = '#fff';
+const STAR_SIZE = 3;
+const STAR_MIN_SCALE = 0.2;
+const OVERFLOW_THRESHOLD = 50;
+const STAR_COUNT = ( window.innerWidth + window.innerHeight ) / 8;
 
-    const scene = new THREE.Scene();
+let canvas
+let context;
+
+let scale = 1, // device pixel ratio
+    width,
+    height;
+
+let stars = [];
+
+let pointerX,
+    pointerY;
+
+let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.002 };
+
+let touchInput = false;
+
+onMount(() => {
+  context = canvas.getContext( '2d' )
+  generate();
+resize();
+step();
+window.onresize = resize;
+// canvas.onmousemove = onMouseMove;
+canvas.ontouchmove = onTouchMove;
+canvas.ontouchend = onMouseLeave;
+document.onmouseleave = onMouseLeave;
+
+})
+
+window.addEventListener("mousemove", onMouseMove)
+
+function generate() {
+
+   for( let i = 0; i < STAR_COUNT; i++ ) {
+    stars.push({
+      x: 0,
+      y: 0,
+      z: STAR_MIN_SCALE + Math.random() * ( 1 - STAR_MIN_SCALE )
+    });
+   }
+
+}
+
+function placeStar( star ) {
+
+  star.x = Math.random() * width;
+  star.y = Math.random() * height;
+
+}
+
+function recycleStar( star ) {
+
+  let direction = 'z';
+
+  let vx = Math.abs( velocity.x ),
+	    vy = Math.abs( velocity.y );
+
+  if( vx > 1 || vy > 1 ) {
+    let axis;
+
+    if( vx > vy ) {
+      axis = Math.random() < vx / ( vx + vy ) ? 'h' : 'v';
+    }
+    else {
+      axis = Math.random() < vy / ( vx + vy ) ? 'v' : 'h';
+    }
+
+    if( axis === 'h' ) {
+      direction = velocity.x > 0 ? 'l' : 'r';
+    }
+    else {
+      direction = velocity.y > 0 ? 't' : 'b';
+    }
+  }
+  
+  star.z = STAR_MIN_SCALE + Math.random() * ( 1 - STAR_MIN_SCALE );
+
+  if( direction === 'z' ) {
+    star.z = 0.1;
+    star.x = Math.random() * width;
+    star.y = Math.random() * height;
+  }
+  else if( direction === 'l' ) {
+    star.x = -OVERFLOW_THRESHOLD;
+    star.y = height * Math.random();
+  }
+  else if( direction === 'r' ) {
+    star.x = width + OVERFLOW_THRESHOLD;
+    star.y = height * Math.random();
+  }
+  else if( direction === 't' ) {
+    star.x = width * Math.random();
+    star.y = -OVERFLOW_THRESHOLD;
+  }
+  else if( direction === 'b' ) {
+    star.x = width * Math.random();
+    star.y = height + OVERFLOW_THRESHOLD;
+  }
+
+}
+
+function resize() {
+
+  scale = window.devicePixelRatio || 1;
+
+  width = window.innerWidth * scale;
+  height = window.innerHeight * scale;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  stars.forEach( placeStar );
+
+}
+
+function step() {
+
+  context.clearRect( 0, 0, width, height );
+
+  update();
+  render();
+
+  requestAnimationFrame( step );
+
+}
+
+function update() {
+
+  velocity.tx *= 0.96;
+  velocity.ty *= 0.96;
+
+  velocity.x += ( velocity.tx - velocity.x ) * 0.8;
+  velocity.y += ( velocity.ty - velocity.y ) * 0.8;
+
+  stars.forEach( ( star ) => {
+
+    star.x += velocity.x * star.z;
+    star.y += velocity.y * star.z;
+
+    star.x += ( star.x - width/2 ) * velocity.z * star.z;
+    star.y += ( star.y - height/2 ) * velocity.z * star.z;
+    star.z += velocity.z;
+  
+    // recycle when out of bounds
+    if( star.x < -OVERFLOW_THRESHOLD || star.x > width + OVERFLOW_THRESHOLD || star.y < -OVERFLOW_THRESHOLD || star.y > height + OVERFLOW_THRESHOLD ) {
+      recycleStar( star );
+    }
+
+  } );
+
+}
+
+function render() {
+
+  stars.forEach( ( star ) => {
+
+    context.beginPath();
+    context.lineCap = 'round';
+    context.lineWidth = STAR_SIZE * star.z * scale;
+    context.globalAlpha = 0.5 + 0.5*Math.random();
+    context.strokeStyle = STAR_COLOR;
+
+    context.beginPath();
+    context.moveTo( star.x, star.y );
+
+    var tailX = velocity.x * 0.5,
+        tailY = velocity.y * 0.5;
+
+    // stroke() wont work on an invisible line
+    if( Math.abs( tailX ) < 0.1 ) tailX = 0.5;
+    if( Math.abs( tailY ) < 0.1 ) tailY = 0.5;
+
+    context.lineTo( star.x + tailX, star.y + tailY );
+
+    context.stroke();
+
+  } );
+
+}
+
+function movePointer( x, y ) {
+
+  if( typeof pointerX === 'number' && typeof pointerY === 'number' ) {
+
+    let ox = x - pointerX,
+        oy = y - pointerY;
+
+    velocity.tx = velocity.tx + ( ox / 32*scale ) * ( touchInput ? 1 : -1 );
+    velocity.ty = velocity.ty + ( oy / 32*scale ) * ( touchInput ? 1 : -1 );
+
+  }
+
+  pointerX = x;
+  pointerY = y;
+
+}
+
+function onMouseMove( event ) {
+
+  touchInput = false;
+
+  movePointer( event.clientX, event.clientY );
+
+}
+
+function onTouchMove( event ) {
+
+  touchInput = true;
+
+  movePointer( event.touches[0].clientX, event.touches[0].clientY, true );
+
+  event.preventDefault();
+
+}
+
+function onMouseLeave() {
+
+  pointerX = null;
+  pointerY = null;
+
+}
+
+</script>
+
+<!-- <script lang="js">
+import * as THREE from "three";
+import { onMount } from "svelte"
+
+let canvas
+
+onMount(() => {
+    createScene(canvas)
+})
+
+const getRandomParticelPos = (particleCount) => {
+  const arr = new Float32Array(particleCount * 3);
+  for (let i = 0; i < particleCount; i++) {
+    arr[i] = (Math.random() - 0.5) * 10;
+  }
+  return arr;
+};
+
+
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+const color = 0xffffff, intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x44aa88})
 const cube = new THREE.Mesh(geometry, material);
 let renderer;
 scene.add(cube);
-camera.position.z = 5;
+
+
+  
+camera.position.z = 5;  
+
+let mouseX = 0;
+let mouseY = 0;
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
 
 const animate = () => {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  
+  cube.rotation.x = mouseY * 0.005;
+  cube.rotation.y = mouseX * 0.005;
     //   renderer.setClearColor(new THREE.Color("#1c1624"));
+
+    
   renderer.render(scene, camera);
+
+  
+
+  requestAnimationFrame(animate);
 };
 
 const resize = () => {
@@ -35,13 +304,14 @@ const resize = () => {
 
 export const createScene = (el) => {
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, canvas: el });
+  
   resize();
   animate();
 }
 
 window.addEventListener('resize', resize);
-</script>
-
+</script> -->
+<!-- 
 <style>
     		canvas {
                 display: block;
@@ -52,4 +322,4 @@ window.addEventListener('resize', resize);
     opacity: 100%;
     transition: opacity 1500ms ease-out;
 		}
-</style>
+</style> -->
