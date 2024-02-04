529fdd1e0e62bd4c4f6763d8299824f7c968dff6
bisaek <59936920+bisaek@users.noreply.github.com>
2024-02-04 22:26:20 +0100

    popover

---
+++ client/src/components/chat/Chat.jsx
@@ -0,0 +1,21 @@
+import chatRead from "@assets/icons/chat-read.svg";
+
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+  } from "@ui/popover"
+  
+
+export default function Chat() {
+    return (
+        <div class=" right-4 bottom-4 fixed">
+            <Popover>
+            <PopoverTrigger><img class="h-16 hover:scale-110 transition" src={chatRead.src}></img></PopoverTrigger>
+            <PopoverContent >
+                <div>hello world</div>
+            </PopoverContent>
+            </Popover>
+        </div>
+    )
+}
