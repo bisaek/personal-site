94138b977edb633d16ee5ede8abae407e2f98426
bisaek <59936920+bisaek@users.noreply.github.com>
2024-02-06 22:47:48 +0100

    fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,5 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind({
     applyBaseStyles: false,
-  }),, svelte(), react()]
+  }), svelte(), react()]
 });
